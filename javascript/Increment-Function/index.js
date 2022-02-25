c4a9ef1a24c2a083313be550995fc422b27ff4ad
Ian <ianzpimentel@gmail.com>
2022-02-25 12:07:10 +0000

    Learning

---
+++ javascript/Increment-Function/index.js
@@ -0,0 +1,15 @@
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function increaseLaps(){
+    lapsCompleted = labsCompleted + 1
+}
+
+lapsCompleted()
+lapsCompleted()
+lapsCompleted()
+
+
+console.log(lapsCompleted)
