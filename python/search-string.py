367bfef04ef596c6514e3146e4c058685fe02607
Ian <ianzpimentel@gmail.com>
2022-02-25 18:29:07 +0000

    Coding2022

--- python/search-string.py
+++ python/search-string.py
@@ -1,4 +1,14 @@
+# Search for position in string
 var = 'cellphone'
 result = var.find('ph')
 
-print(result) # Returns 4
+print(result) # 4
+
+# Search and replace strings
+greet = 'Hello Ian'
+
+chStr = greet.replace('Ian','Paloma')
+print(chStr) # Hello Paloma
+
+chStr = greet.replace('o','e')
+print(chStr) # Helle Ian
