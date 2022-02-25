a1864ff9351f8880984b91c1a706d2ffd73b5d36
Ian <ianzpimentel@gmail.com>
2022-02-25 14:31:23 +0000

    JS-Learning

---
+++ javascript/metro-count-app/index.js
@@ -0,0 +1,34 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let welcomeEl = document.getElementById("welcome-el")
+let countEl = document.getElementById("count-el")
+
+
+
+function increment(){
+    count += 1
+    countEl.innerHTML = count
+}
+
+
+// Save last entries
+
+function save(){
+    let lastCount = count + " - "
+    saveEl.innerHTML += lastCount
+    count = 0
+    countEl.innerHTML = count
+}
+
+// Add welcoming greet
+
+name = "Ian Zdanowsky"
+greetings = "Welcome back, "
+
+welcomeEl.innerHTML = greetings + name
+welcomeEl.innerHTML += "!"
+
+//
+
+
+
