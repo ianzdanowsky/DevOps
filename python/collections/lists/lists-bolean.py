51baeca73bff4218c2bc42b3c0b789e912c86f68
Ian <ianzpimentel@gmail.com>
2022-02-26 02:31:55 +0000

    Coding2022

---
+++ python/collections/lists/lists-bolean.py
@@ -0,0 +1,7 @@
+myList = [1, 'ian', 3, 3.99]
+
+x = 'ian' in myList # Verify if 'ian' is inside myList list
+print(x)  # True or false statement
+
+y = 9 in myList
+print(y)
---
+++ python/collections/lists/string-to-lists.py
@@ -0,0 +1,6 @@
+myString = 'I love you baby'
+
+myStringList = myString.split() # turns multiple words in string to list
+
+print(myStringList) # [I, love, you, baby]
+print(myStringList[1]) # print love
